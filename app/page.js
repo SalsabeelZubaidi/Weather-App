33b51c112323310744be7ac66e6588ce7ac1ddf1
SalsabeelZubaidi <158805325+SalsabeelZubaidi@users.noreply.github.com>
2026-01-18 14:41:50 +0300

    complete static UI layout

--- app/page.js
+++ app/page.js
@@ -3,14 +3,12 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 
 export default function Home() {
-  return (
-    <div className="px-40 py-12">
-      {/* main container */}
-      <div className="mx-auto w-[888px] flex flex-col gap-6">
-        {/* Header */}
-        <div className="flex justify-between items-center">
-          <img src="/images/weatherAppLogo.png" alt="Waether App"/>
+  return (<>
 
+      {/* Header */}
+      <div className="pt-10 pr-5 pl-5 pb-5">
+        <div className="flex justify-between items-center">
+          <img src="/images/weatherAppLogo.png" alt="Waether App" className="w-[144px] h-[24px]"/>
           <div className="flex items-center">
             <h4 className="font-bold mr-5 text-[18px]">°C</h4>
             <img
@@ -19,9 +17,11 @@ export default function Home() {
             />
           </div>
         </div>
+      </div>  
+        <hr className="opacity-50 w-full" />
 
-        <hr className="opacity-50" />
-
+      {/* body container */}  
+      <div className="pt-10 pr-30 pb-20 pl-30 flex flex-col gap-6">
         {/* Search bar */}
         <div className="flex items-center bg-[#26303B] rounded-2xl h-[72px] px-8">
           <FontAwesomeIcon
@@ -34,8 +34,8 @@ export default function Home() {
         </div>
 
         {/* Location title  */}
-        <div className="flex justify-center pt-24 pr-16 pb-12 pl-16">
-          <h1 className="text-xl font-bold text-[32px] ">
+        <div className="flex justify-center pt-24 pr-16  pl-16">
+          <h1 className="text-xl font-bold text-[40px] ">
           Amman, Jordan
         </h1>
         </div>
@@ -44,9 +44,83 @@ export default function Home() {
           <img src='/images/sun.png'/>
           <span className="text-[24px]"> Mostly clear with a high of 75F</span>
         </div>
-        
+        {/* foreacst statictics */}
+        <div className="flex flex-row gap-8 justify-center">
+          <div className="flex flex-col items-start p-10 bg-[#26303B]  w-[455px] h-[200px] rounded-2xl gap-4">
+            <h4 className="text-[24px]">Humidity</h4>
+            <h3 className="text-[28px] font-bold">60%</h3>
+            <h5 className="text-[16px] text-[#99ABBD]">Cloud</h5>
+          </div>
+          <div className="flex flex-col items-start p-10 bg-[#26303B]  w-[455px] h-[200px] rounded-2xl gap-4">
+            <h4 className="text-[24px]">Wind</h4>
+            <h3 className="text-[28px] font-bold">5 mph</h3>
+            <h5 className="text-[16px] text-[#99ABBD]">Wind</h5>
+          </div>
+          <div className="flex flex-col items start bg-[#26303B] p-10 w-[455px] h-[200px] rounded-2xl gap-4">
+            <h4 className="text-[24px]">Feels Like</h4>
+            <h3 className="text-[28px] font-bold">72F</h3>
+            <h5 className="text-[16px] text-[#99ABBD]">Thermometer</h5>
+          </div>
+        </div>
+        {/*  forecats Days table */}
+        <div className="inline">
+          <h3 className="mt-15 mb-[-50px] text-3xl font-bold">5-Day Foreacst</h3>
+        </div>
+          <table className="mt-15 rounded-xl overflow-hidden">          
+          <thead className="border border-gray-700 bg-[#1C2129]">
+            <tr>
+                <td className="p-4">
+                  Day
+                </td>
+                <td>
+                  High/Low
+                </td>
+                <td>
+                  Condition
+                </td>
+                <td>
+                </td>
+              </tr>
+            </thead>
+            <tbody className="border border-[#E5E8EB]/10 border-opa">
+              <tr className="border-t border-b border-[#E5E8EB]">
+                <td className="p-7">
+                  Monday
+                </td>
+                <td>
+                  78F/ 65F
+                </td>
+                <td>
+                  Partly Cloudy
+                </td>
+                <td>
+                  <img className='w-[40px] h-[40px]' src='/images/sun.png' />
+                </td>
+              </tr>
+            </tbody>
+            <tbody>
+              <tr className="border-t border-b border-[#E5E8EB]">
+                <td className="p-7">
+                  Monday
+                </td>
+                <td>
+                  78F/ 65F
+                </td>
+                <td>
+                  Partly Cloudy
+                </td>
+                <td>
+                  <img className='w-[40px] h-[40px]' src='/images/sun.png' />
+                </td>
+              </tr>
+            </tbody>  
+          </table>
+        </div>    
 
-      </div>
-    </div>
+      {/* footer */}
+      <footer className="p-10 flex justify-center text-[20px] text-[#99ABBD]">
+        © 2026 Weather App. All rights reserved.
+      </footer>
+      </>
   );
 }
